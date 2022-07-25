105539298a6605a367169d8c26bafbe39bf29ddb
JuanjoChiliutti <juanjo.chiliutti@gmail.com>
2022-07-25 16:21:20 -0300

    webvitals fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,12 +12,11 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
